23095193e6bbbfd7f24c228168f35f2343baeb35
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-04 22:16:50 -0500

    commit tree d12359f6ba67134f1f0a33dcad74b66b749fe9cd

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,66 @@
+require "pry"
+
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && (password == "12345")
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
+
+admin_login "sudo", "12345"
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40 
+    "It's brisk out there!"
+  elsif temperature < 64 && temperature > 40 
+    "It's a little chilly out there!"
+  elsif temperature > 85 
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
+  end
 end
+
+hows_the_weather(30)
+hows_the_weather(45)
+hows_the_weather(79)
+hows_the_weather(90)
 
 def fizzbuzz(num)
-  # your code here
+  if (num % 3 == 0) && (num % 5 == 0)
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  else 
+    num
+  end
 end
 
+fizzbuzz(1)
+fizzbuzz(2)
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+
+
+
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "*" then num1 * num2
+  when "/" then num1 / num2
+  else puts "Invalid operation!"
+  end
 end
+
+calculator("+", 1, 1)
+calculator("-", 1, 1)
+calculator("*", 1, 1)
+calculator("/", 1, 1)
+calculator("none", 1, 1)
+
 
